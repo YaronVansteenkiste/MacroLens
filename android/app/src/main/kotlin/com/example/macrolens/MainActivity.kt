e18f570f5522cb451c3afe63f4289bde3df01ae4
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2025-01-31 17:06:36 +0100

    31/05/2025 - new project

---
+++ android/app/src/main/kotlin/com/example/macrolens/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.macrolens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MacroLens'),
+          centerTitle: true,
+          backgroundColor: Colors.blue[600],
+        ),
+        body: Center(
+          child: Text(
+            'Coming Soon...',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue[600],
+              fontFamily: 'Roboto',
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
