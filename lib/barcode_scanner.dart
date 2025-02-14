3f1e500d8927abb99a38227bd7723d04a253ae9d
Yaron Vansteenkiste <125354929+YaronVansteenkiste@users.noreply.github.com>
2025-02-14 11:30:48 +0100

    14/02: scanning barcode

--- lib/barcode_scanner.dart
+++ lib/barcode_scanner.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
-class BarcodeScanner extends StatelessWidget {
+class BarcodeScanner extends StatefulWidget {
   const BarcodeScanner({super.key});
 
-   @override
+  @override
+  State<BarcodeScanner> createState() => _BarcodeScannerState();
+}
+
+class _BarcodeScannerState extends State<BarcodeScanner> {
+  String result = "No result";
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Barcode Scanner TODO'),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                String? res = await SimpleBarcodeScanner.scanBarcode(
+                  context,
+                  barcodeAppBar: const BarcodeAppBar(
+                    appBarTitle: 'Test',
+                    centerTitle: false,
+                    enableBackButton: true,
+                    backButtonIcon: Icon(Icons.arrow_back_ios),
+                  ),
+                  isShowFlashIcon: true,
+                  delayMillis: 500,
+                  cameraFace: CameraFace.back,
+                  scanFormat: ScanFormat.ONLY_BARCODE,
+                );
+                setState(() {
+                  result = res ?? "No result";
+                });
+              },
+              child: const Text('Scan Barcode'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text('Scan Barcode Result: $result'),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
