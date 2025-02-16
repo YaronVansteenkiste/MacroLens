804bbe43d26e343a02cd9bf154c5985b0a2365b0
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2025-02-16 15:40:52 +0100

    16/02: add space for titlebar (ios)

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 21, 27, 35),
+      ),
       body: PersistentTabView(
         tabs: [
           PersistentTabConfig(
